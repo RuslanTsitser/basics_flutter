0ac76e006f42618570c2e2ea1aa389a4ea233ad1
RuslanTsitser <cicer_ruslan@mail.ru>
2025-06-13 15:16:10 +0300

    Implement characters screen with favorites functionality, including character loading, favorite management, and UI updates for displaying characters and favorites.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'all_in_one_file/characters_screen_0_stateful.dart' as stateful;
+import 'characters_with_favorites/characters_with_favorites_0_stateful.dart' as stateful;
 
 void main() {
   runApp(const stateful.MainApp());
