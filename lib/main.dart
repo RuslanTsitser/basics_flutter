8dcdd68d9281f10796b273dd929f7830a43ec1e1
RuslanTsitser <cicer_ruslan@mail.ru>
2025-06-13 15:20:07 +0300

    Update character loading implementation and introduce a new main file for characters with favorites functionality. Refactor imports and add caching for character data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'characters_with_favorites/characters_with_favorites_0_stateful.dart' as stateful;
+import 'characters_with_favorites/main_00.dart' as stateful;
 
 void main() {
   runApp(const stateful.MainApp());
