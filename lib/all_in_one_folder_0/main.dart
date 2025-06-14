c3727c264f7d4115ed567dfe1c999a354929f4f9
RuslanTsitser <cicer_ruslan@mail.ru>
2025-06-14 14:35:20 +0300

    Add character management features with UI components for displaying characters and favorites
    Introduce a new Character class for data representation, along with UI components including CharactersList, FavoritesList, CharactersAppBar, and CharactersBottomNavigationBar. Implement a CharactersNotifier for state management, enabling character loading, favorite management, and improved user interaction through a structured layout. This commit enhances the overall functionality and maintainability of the character management system.

---
+++ lib/all_in_one_folder_0/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'characters_app_bar.dart';
+import 'characters_bottom_navigation_bar.dart';
+import 'characters_list.dart';
+import 'characters_notifier.dart';
+import 'characters_notifier_provider.dart';
+import 'favorites_list.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final CharactersNotifier notifier = CharactersNotifier();
+
+  @override
+  void initState() {
+    super.initState();
+    notifier.loadCharacters();
+    notifier.loadFavorites();
+    notifier.initListeners();
+  }
+
+  @override
+  void dispose() {
+    notifier.disposeControllers();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CharactersNotifierProvider(
+      notifier: notifier,
+      child: const Scaffold(
+        appBar: CharactersAppBar(),
+        body: CharactersBody(),
+        bottomNavigationBar: CharactersBottomNavigationBar(),
+      ),
+    );
+  }
+}
+
+class CharactersBody extends StatelessWidget {
+  const CharactersBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final notifier = CharactersNotifierProvider.of(context)!.notifier!;
+    return notifier.selectedIndex == 0 ? const CharactersList() : const FavoritesList();
+  }
+}
