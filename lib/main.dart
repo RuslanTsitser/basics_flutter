360a6d431a111ecb6b547994a304b1a8fd55dcc3
RuslanTsitser <cicer_ruslan@mail.ru>
2025-06-14 14:40:48 +0300

    Refactor character management system by introducing a repository pattern for improved data handling and separation of concerns. Update UI components and state management with CharactersNotifier, enhancing maintainability and user experience. This commit lays the groundwork for a more structured and efficient character management application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
-import 'characters_with_favorites/main_00.dart' as stateful;
+import '2.one_folder_with_repositories/main.dart' as stateful;
 
 void main() {
-  runApp(const stateful.MainApp());
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: stateful.MainScreen(),
+    );
+  }
 }
