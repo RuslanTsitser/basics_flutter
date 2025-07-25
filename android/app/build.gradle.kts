e668e5fc7560cb588306af6acf2819ceaa45da11
RuslanTsitser <cicer_ruslan@mail.ru>
2025-07-25 21:55:31 +0300

    Update NDK version in build.gradle.kts to 27.0.12077973 for compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.tsitser.dev.basics_flutter"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
